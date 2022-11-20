2fd279021569e2084f7ee04eb3b39348491eb8d3
NwaguChima <nwaguchima48@gmail.com>
2022-11-20 14:54:34 +0100

    setup context

--- src/theme.js
+++ src/theme.js
@@ -192,3 +192,25 @@ export const themeSettings = (mode) => {
     },
   };
 };
+
+// context for color mode
+export const colorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return [theme, colorMode];
+};
