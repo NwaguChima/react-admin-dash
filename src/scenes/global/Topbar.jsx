9d89fbcc9dc9851875c9c7b7e5614885b073fade
NwaguChima <nwaguchima48@gmail.com>
2022-11-20 15:44:24 +0100

    added input

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,7 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-import InputBase from "@mui/material";
+import { InputBase } from "@mui/material";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
@@ -14,7 +14,21 @@ const Topbar = () => {
   const colors = tokens(theme.palette.mode);
   const { colorMode, setColorMode } = useContext(ColorModeContext);
 
-  return <Box>Hello</Box>;
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* Search BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default Topbar;
