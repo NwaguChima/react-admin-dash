904d89357f28c9b9204225e1a91e5a584fcc9fb8
NwaguChima <nwaguchima48@gmail.com>
2022-11-20 16:56:52 +0100

    updated styles for react pro sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -18,7 +18,36 @@ import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 const Sidebar = () => {
-  <div>Sidebar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[500]} !important`,
+        },
+
+        "& .pro-icon-wrapper": {
+          backgroundColor: `transparent !important`,
+        },
+
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+
+        "& .pro-menu-item.active": {
+          color: "#868dfb !important",
+        },
+      }}
+    ></Box>
+  );
 };
 
 export default Sidebar;
