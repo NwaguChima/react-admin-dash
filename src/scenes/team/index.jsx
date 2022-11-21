d37139aba7f28cc8a62043bde8d3552aca2062c0
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 02:24:12 +0100

    setup imports for teams

---
+++ src/scenes/team/index.jsx
@@ -0,0 +1,8 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from "../../components/Header";
