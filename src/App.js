e06b20b3bf2edd0e4f4c9ae4a6d6b93ac29b3b4f
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 17:49:39 +0100

    completed accordion

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Calendar from "./scenes/calendar";
 import Form from "./scenes/form";
+import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 
 function App() {
@@ -33,10 +33,10 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
