b2a186f009b3891e898d83e07c8e60805d85dcbd
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 17:43:11 +0100

    completed full calendar implementation

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -47,7 +47,7 @@ const Calendar = () => {
   };
 
   return (
-    <Box mt="20px">
+    <Box m="20px">
       <Header title="Calendar" subtitle="Full Calendar Interactive Page" />
 
       <Box display="flex" justifyContent="space-between">
@@ -85,6 +85,44 @@ const Calendar = () => {
               </ListItem>
             ))}
           </List>
+        </Box>
+
+        {/* Calendar */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: "12315",
+                title: "All-day event",
+                date: "2022-11-14",
+              },
+              {
+                id: "5123",
+                title: "Timed event",
+                date: "2022-11-28",
+              },
+            ]}
+          />
         </Box>
       </Box>
     </Box>
