88c5cd7f4d7df05117b519db8f638c57bd40a964
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 18:01:20 +0100

    added bar to page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contacts from "./scenes/contacts";
 import Calendar from "./scenes/calendar";
 import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import Geography from "./scenes/geography";
@@ -34,7 +34,7 @@ function App() {
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/form" element={<Form />} />
               <Route path="/faq" element={<FAQ />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
+              <Route path="/bar" element={<Bar />} />
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
