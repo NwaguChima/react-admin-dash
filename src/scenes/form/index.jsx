1c677daef7d1d42957e113d824c337062f607231
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 17:11:19 +0100

    setup formik and yup

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -4,6 +4,47 @@ import * as Yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
-const Form = () => {};
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+};
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const userSchema = Yup.object().shape({
+  firstName: Yup.string().required("First Name is required"),
+  lastName: Yup.string().required("Last Name is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  contact: Yup.string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("Contact is required"),
+  address1: Yup.string().required("Address is required"),
+  address2: Yup.string().required("Address is required"),
+});
+
+const Form = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="CREATE USER" subtitle="Create a New User Profile" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={userSchema}
+      ></Formik>
+    </Box>
+  );
+};
 
 export default Form;
