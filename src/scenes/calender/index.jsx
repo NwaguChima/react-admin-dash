ec4e000919ff2f8c55bffa82ec9065ea8a47703b
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 17:29:30 +0100

    added handleDateClick function

--- src/scenes/calender/index.jsx
+++ src/scenes/calender/index.jsx
@@ -14,3 +14,27 @@ import {
 } from "@mui/material";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
+
+const Calendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+};
+
+export default Calendar;
