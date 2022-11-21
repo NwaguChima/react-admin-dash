833a63b888029991a438613e6b720248f7b825bb
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 17:25:37 +0100

    added calender imports

---
+++ src/scenes/calender/index.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
