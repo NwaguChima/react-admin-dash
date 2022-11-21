d64b848605b7027e1bc558e6947cf505d9785454
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 17:51:00 +0100

    completed accordion

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -10,7 +10,6 @@ import { tokens } from "../../theme";
 const FAQ = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-
   return (
     <Box m="20px">
       <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
