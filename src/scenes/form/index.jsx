de82572d907e112111a77dbfcadd1c1855d03dbe
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 17:04:31 +0100

    setup form

---
+++ src/scenes/form/index.jsx
@@ -0,0 +1,9 @@
+import { Box, Button, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+
+const Form = () => {};
+
+export default Form;
