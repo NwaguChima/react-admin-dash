6a579488f063e83b3e18caeee06766f63438cdec
NwaguChima <nwaguchima48@gmail.com>
2022-11-21 17:45:25 +0100

    added faq imports

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,8 @@
+import { Box, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
