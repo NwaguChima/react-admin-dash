3967a832831a7888471850fbe35fe21e92601fe8
NwaguChima <nwaguchima48@gmail.com>
2022-11-22 06:37:38 +0100

    created pie index file

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import Calendar from "./scenes/calendar";
 import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
 // import Geography from "./scenes/geography";
 
 function App() {
@@ -35,8 +35,8 @@ function App() {
               <Route path="/form" element={<Form />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
               {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
